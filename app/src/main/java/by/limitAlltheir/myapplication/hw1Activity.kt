0f9a58e12e5e3161ff8f383282e732c916ad553d
limitAlltheir <nikitakozkoz1999@gmail.com>
2020-09-20 02:48:13 +0300

    Start point

--- app/src/main/java/by/limitAlltheir/myapplication/hw1Activity.kt
+++ app/src/main/java/by/limitAlltheir/myapplication/hw1Activity.kt
@@ -1,11 +1,23 @@
 package by.limitAlltheir.myapplication
 
+import android.icu.number.NumberFormatter.with
+import android.icu.number.NumberRangeFormatter.with
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_hw1.*
 
 class hw1Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hw1)
+
+        buttonLoad.setOnClickListener {
+            var url = editText.text.toString()
+            val pic = Picasso.get()
+            pic.isLoggingEnabled = true
+            pic.load(url).into(this.imageView)
+        }
     }
 }
