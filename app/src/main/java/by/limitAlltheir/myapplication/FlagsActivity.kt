c8910da17399cddc756d209d4a5e0d2ddfd430e4
limitAlltheir <nikitakozkoz1999@gmail.com>
2020-09-23 22:53:44 +0300

    Create activity flags. Add listener to the FLAGS button.

---
+++ app/src/main/java/by/limitAlltheir/myapplication/FlagsActivity.kt
@@ -0,0 +1,11 @@
+package by.limitAlltheir.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class FlagsActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_flags)
+    }
+}
--- app/src/main/java/by/limitAlltheir/myapplication/Hw3Activity.kt
+++ app/src/main/java/by/limitAlltheir/myapplication/Hw3Activity.kt
@@ -1,11 +1,18 @@
 package by.limitAlltheir.myapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_hw3.*
 
 class Hw3Activity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_hw3)
+
+        buttonFlags.setOnClickListener {
+            val myIntent = Intent(this, FlagsActivity::class.java)
+            startActivity(myIntent)
+        }
     }
 }
