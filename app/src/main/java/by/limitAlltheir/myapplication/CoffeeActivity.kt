a8d976785cf13292fabff17eba44a880867b24e8
limitAlltheir <nikitakozkoz1999@gmail.com>
2020-09-30 15:04:21 +0300

    Add button to the MainActivity leading to the CoffeeActivity.

---
+++ app/src/main/java/by/limitAlltheir/myapplication/CoffeeActivity.kt
@@ -0,0 +1,11 @@
+package by.limitAlltheir.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class CoffeeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_coffee)
+    }
+}
