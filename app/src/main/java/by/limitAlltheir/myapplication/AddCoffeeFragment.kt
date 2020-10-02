944a30f0d28d49314d14164242db11159df6723f
limitAlltheir <nikitakozkoz1999@gmail.com>
2020-10-02 17:48:27 +0300

    Add functional for show collections.

--- app/src/main/java/by/limitAlltheir/myapplication/AddCoffeeFragment.kt
+++ app/src/main/java/by/limitAlltheir/myapplication/AddCoffeeFragment.kt
@@ -4,6 +4,8 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageSwitcher
+import android.widget.ImageView
 import android.widget.Toast
 import androidx.fragment.app.Fragment
 import kotlinx.android.synthetic.main.fragment_add_coffee.*
