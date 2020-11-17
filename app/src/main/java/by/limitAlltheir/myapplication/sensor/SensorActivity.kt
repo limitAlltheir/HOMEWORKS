ef01014b708ca6e0f3634329ecc510f73fc48837
limitAlltheir <nikitakozkoz1999@gmail.com>
2020-11-17 15:01:59 +0300

    Create light sensor as object.

--- app/src/main/java/by/limitAlltheir/myapplication/sensor/SensorActivity.kt
+++ app/src/main/java/by/limitAlltheir/myapplication/sensor/SensorActivity.kt
@@ -1,7 +1,6 @@
 package by.limitAlltheir.myapplication.sensor
 
 import android.content.Context
-import android.graphics.Color
 import android.graphics.Color.GREEN
 import android.graphics.Color.RED
 import android.hardware.Sensor
@@ -9,16 +8,15 @@ import android.hardware.Sensor.TYPE_LIGHT
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
 import android.hardware.SensorManager
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Layout
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
-import androidx.core.content.getSystemService
 import by.limitAlltheir.myapplication.R
 
-class SensorActivity : AppCompatActivity(), SensorEventListener {
+class SensorActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sensor)
@@ -29,26 +27,31 @@ class SensorActivity : AppCompatActivity(), SensorEventListener {
 
         val buttonLight = findViewById<Button>(R.id.button_light)
 
-        buttonLight.setOnClickListener {
+        buttonLight.setOnClickListener(object : SensorEventListener, View.OnClickListener {
+            override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            }
 
-            sensorManager.registerListener(this, lightSensor, 1)
-        }
+            override fun onSensorChanged(event: SensorEvent?) {
+                val layoutSensor = findViewById<ConstraintLayout>(R.id.sensor_layout)
+                val textLight = findViewById<TextView>(R.id.text_view_light)
+                textLight.text = event?.values?.get(0).toString()
+                val levelLight = event?.values?.get(0)?.toInt() ?: 0
 
-    }
+                if (levelLight >= 300) {
+                    layoutSensor.setBackgroundColor(GREEN)
+                } else {
+                    layoutSensor.setBackgroundColor(RED)
+                }
+            }
 
-    override fun onAccuracyChanged(sensor: Sensor?, accuracy: Int) {
+            override fun onClick(v: View?) {
+                sensorManager.registerListener(this, lightSensor, 1)
+            }
 
-    }
+        })
 
-    override fun onSensorChanged(event: SensorEvent?) {
-        val layoutSensor = findViewById<ConstraintLayout>(R.id.sensor_layout)
-        val textLight = findViewById<TextView>(R.id.text_view_light)
-        textLight.text = event?.values?.get(0).toString()
 
-        if (event?.values?.get(0)?.toInt()!! >= 300) {
-            layoutSensor.setBackgroundColor(GREEN)
-        } else {
-            layoutSensor.setBackgroundColor(RED)
-        }
+
+
     }
 }
